6d909eabac0a90d2806fe756bba1634903a61c60
stman3 <75269916+stman3@users.noreply.github.com>
2022-05-03 13:00:25 +0300

    fix erros.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
-import React, {useState,useEffect} from "react"
+import React, {useState} from "react"
 import io from 'socket.io-client'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './Pages/Home'
 import Game from './Pages/Game'
 import Lobby from './Pages/Lobby'
 import ErrorPage from './Pages/ErrorPage'
 
-
-
+const socket = io.connect('http://192.168.8.122:3001')
+const player=null
 
 const App =()=> {
-
+  const {players,Setplayers} = useState([])
   return (
    <Router>
      <Routes>
-       <Route path='/' element={<Home/>}/>
-       <Route path='/Lobby' element={<Lobby/>}/>
-       <Route path='/Game' element={<Game/>}/>
+       <Route path='/' element={<Home socket={socket} player={player}/>}/>
+       <Route path='/Lobby' element={<Lobby socket={socket} players={players} Setplayers={Setplayers} player={player}/>}/>
+       <Route path='/Game' element={<Game socket={socket} players={players} Setplayers={Setplayers} player={player}/>}/>
        <Route path='*' element={<ErrorPage/>}/>
      </Routes>
    </Router>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, {useState,useEffect} from "react"
 import { useNavigate } from "react-router-dom";
 
 
+const enterRoom=(PlayerName,playerRoom,navigate,props)=>{
+    if(PlayerName&&playerRoom){
+        const newPlayer ={
+            PlayerName:PlayerName,
+            playerPoint:0,
+            playerRank:'admin',
+            playerRoom:playerRoom
+        }
+        props.socket.emit('player',newPlayer)
+    }
+    else(
+        alert('Name is empty Or The Room number is worng!')
+    )
+}
 
 
+const createRoom =(PlayerName,navigate,props)=>{
+    navigate('/lobby')
+   
+}
 
-const Home =()=>{
+const Home =(props)=>{
     let navigate = useNavigate();
+    const [PlayerName,SetPalyerName] = useState('')
+    const [playerRoom,SetplayerRoom] = useState('')
+
+
+    const  handleNumberChange=(event)=>{
+        console.log(`number: ${event.target.value}`)
+        SetPalyerName(event.target.value)
+      }
+
+      const  handleRoomChange =(event)=>{
+        console.log(`Room: ${event.target.value}`)
+        SetplayerRoom(event.target.value)
+      }
+
+
     return(
         <div>
-        <div>Home</div>
-        <button onClick={()=>navigate('/Lobby')}>Go to the Lobby Page</button>
+            <h1>UserName</h1>
+            <input value={PlayerName} placeholder="Enter UserName" onChange={handleNumberChange} />
+            <input value={playerRoom} placeholder="Enter The Number Room" onChange={handleRoomChange}/>
+            <button onClick={()=> enterRoom(PlayerName,playerRoom,navigate,props)}>Enter The Room</button>
+            <button onClick={()=> createRoom (PlayerName,navigate,props)}>Create Room</button>
         </div>
     )
 }
+
+
+
+
 
 
 
