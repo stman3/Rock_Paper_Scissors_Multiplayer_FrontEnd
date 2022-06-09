e4a79c395f95310d1255df28d62db24a58f78839
stman3 <75269916+stman3@users.noreply.github.com>
2022-06-10 01:10:34 +0300

    the chat have been finish .

--- src/Pages/Lobby.js
+++ src/Pages/Lobby.js
@@ -1,4 +1,4 @@
-import React, {} from "react"
+import React, {useState,useEffect} from "react"
 //import { useNavigate } from "react-router-dom";
 import Lobbybutton from "../componet/Lobbybutton";
 
@@ -7,7 +7,8 @@ import Lobbybutton from "../componet/Lobbybutton";
 
 const Lobby =(props)=>{
     //let navigate = useNavigate()
-
+    const [messages,setmessages] = useState([])
+    const [newMessage,setMessage] = useState('')
 
         props.socket.on("PlayerRoom",(data)=>{
             props.Setplayers(data.Players)
@@ -17,8 +18,28 @@ const Lobby =(props)=>{
             console.log("afterstate")
         })
 
+        const sendMessage = ()=>{
+            props.socket.emit('send_message',{
+                PlayerRoomNo:props.playerRoomNo,
+                PlayerName:props.player.PlayerName,
+                Message:newMessage
+
+            })
+        }
+
+
+        useEffect(()=>{
+            props.socket.on('receive_message',(data)=>{
+                setmessages((list)=>[...list,data])
+            })
+          },[props.socket]) 
+
+
+
         //<li key={p.socketID}><div id="Playerli">{p.playerState}</div> {p.PlayerName}</li>
-        
+        const handleMessageChange = (event)=>{
+            setMessage(event.target.value)
+          }
 
     return(
         <div>
@@ -36,7 +57,12 @@ const Lobby =(props)=>{
                 </ul>
             </div>
             <Lobbybutton Rank={props.player.playerRank}/>
-            
+            <div id="Contener_Chat">
+                <div id="Contener_Message">{messages.map((mes)=>{
+                    return <p id="Message">{mes.PlayerName}: {mes.Message}</p>
+                })}</div>
+                <input placeholder="Message..." onChange={handleMessageChange}/><button onClick={sendMessage}>Send</button>
+            </div>
         </div>
     )
 }
