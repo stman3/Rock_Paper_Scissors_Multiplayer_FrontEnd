285163cb60ff9c58505bfa954e5be00ee5a97fdb
stman3 <75269916+stman3@users.noreply.github.com>
2022-06-18 04:05:49 +0300

    still need css.

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const CH=[]
-let Pch
+var Pch
 const Game = (props) => {
 
     useEffect(() => {
@@ -10,16 +10,46 @@ const Game = (props) => {
             if(CH.length>1){
                 if((CH[0]==="Rock"&&CH[1]==="Rock")||(CH[0]==="Scissors"&&CH[1]==="Scissors")||(CH[0]==="Paper"&&CH[1]==="Paper")){
                     console.log('Drwa')
+                    alert("draw")
+                    CH.pop()
+                    CH.pop() 
+                }else if((CH[0]==="Rock"&&CH[1]==="Paper")||(CH[0]==="Paper"&&CH[1]==="Rock")){
+                    if(Pch==="Paper"){
+                        alert("You win")
+                    }else{
+                        alert("You lose")
+                    }
+                    CH.pop()
+                    CH.pop() 
+                }else if((CH[0]==="Rock"&&CH[1]==="Scissors")||(CH[0]==="Scissors"&&CH[1]==="Rock")){
+                    console.log('Rock')
+                    if(Pch==="Rock"){
+                        alert("You win")
+                    }else{
+                        alert("You lose")
+                    }
+                    CH.pop()
+                    CH.pop() 
+                }else if((CH[0]==="Paper"&&CH[1]==="Scissors")||(CH[0]==="Scissors"&&CH[1]==="Paper")){
+                    if(Pch==="Scissors"){
+                        alert("You win")
+                    }else{
+                        alert("You lose")
+                    }
+                    CH.pop()
+                    CH.pop() 
+                }
+                if(props.player.playerRank==="admin"){
                     const btn = document.createElement("button")
-                    btn.innerHTML = "Click Me";
+                    btn.innerHTML = "Restart ";
                     btn.addEventListener('click',restart)
                     const ds = document.getElementById("s")
                     ds.appendChild(btn)
-                    CH.pop()
-                    CH.pop()
-                    
                 }
             }
+        })
+        props.socket.on('receive_Restart',(data)=>{
+            restarts()
         })
     }, [props.socket])
     const restart=()=>{
@@ -29,6 +59,18 @@ const Game = (props) => {
         })
         const ds = document.getElementById("s")
         ds.removeChild(ds.firstChild)
+        props.socket.emit('send_Restart',{
+            PlayerRoomNo:props.playerRoomNo,
+            PlayerCh: "Scissors"
+        })
+    }
+
+    const restarts=()=>{
+        console.log('restartsss')
+        const hpBtns = document.querySelectorAll(".endbtn button");
+        hpBtns.forEach(button=>{
+            button.disabled=false
+        })
     }
     const Scissors =()=>{
         const hpBtns = document.querySelectorAll(".endbtn button");
